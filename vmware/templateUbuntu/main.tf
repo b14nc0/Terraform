48d28c99c75bfdc852038f7e3eb45a1a3b5be4f8
b14nc0 <42178316+b14nc0@users.noreply.github.com>
2022-12-27 10:52:30 +0100

    Update main.tf

--- vmware/templateUbuntu/main.tf
+++ vmware/templateUbuntu/main.tf
@@ -1,48 +1,96 @@
-provider "vsphere" {
-  user           = ""
-  password       = ""
-  vsphere_server = ""
-# If you have a self-signed cert
-  allow_unverified_ssl = true
-}
 data "vsphere_datacenter" "dc" {
-  name = var.vsphere_datacenter
+  name = "${var.vsphere_datacenter}"
 }
+
+data "vsphere_datastore_cluster" "datastore_cluster" {
+  count         = var.vsphere_datastore_cluster != "" ? 1 : 0
+  name          = var.vsphere_datastore_cluster
+  datacenter_id = data.vsphere_datacenter.dc.id
+}
+
 data "vsphere_datastore" "datastore" {
-  name          = ""
+  count         = var.vsphere_datastore != "" && var.vsphere_datastore_cluster == "" ? 1 : 0
+  name          = var.vsphere_datastore
   datacenter_id = data.vsphere_datacenter.dc.id
 }
+
+data "vsphere_resource_pool" "pool" {
+  name          = var.vsphere_resource_pool
+  datacenter_id = data.vsphere_datacenter.dc.id
+}
+
 data "vsphere_network" "network" {
-  name          = ""
-  datacenter_id = data.vsphere_datacenter.dc.id
-}
-data "vsphere_virtual_machine" "source_template" {
-  name          = "" 
-  datacenter_id = data.vsphere_datacenter.dc.id
-}
-data "vsphere_compute_cluster" "cluster" {
-  name          = ""
+  count         = var.network_cards != null ? length(var.network_cards) : 0
+  name          = var.network_cards[count.index]
   datacenter_id = data.vsphere_datacenter.dc.id
 }
 
+data "vsphere_virtual_machine" "template" {
+  name          = "${var.vsphere_vm_template_name}"
+  datacenter_id = "${data.vsphere_datacenter.dc.id}"
+}
 
-resource "vsphere_virtual_machine" "templateUbuntu" {
-  name             = "MVPRUEBATERRAFORM"
-  resource_pool_id = data.vsphere_compute_cluster.cluster.resource_pool_id
-  datastore_id     = data.vsphere_datastore.datastore.id
-  folder           = "Pruebas"
-  num_cpus = 2
-  memory   = 1024
-  guest_id = "other3xLinux64Guest"
-  cpu_hot_add_enabled    = true 
-  memory_hot_add_enabled = true
-  scsi_type = data.vsphere_virtual_machine.source_template.scsi_type
-  network_interface {
-    network_id = data.vsphere_network.network.id
+locals {
+  interface_count     = length(var.ipv4_submask) #Used for Subnet handeling
+  template_disk_count = length(data.vsphere_virtual_machine.template.disks)
+}
+
+// Provisioning Ubuntu Server from the VM template
+resource "vsphere_virtual_machine" "arcdemo" {
+  count                  = var.vsphere_virtual_machine_count
+  name                   = var.vsphere_virtual_machine_name
+  resource_pool_id       = data.vsphere_resource_pool.pool.id
+  folder                 = var.vsphere_folder
+  datastore_cluster_id   = var.vsphere_datastore_cluster != "" ? data.vsphere_datastore_cluster.datastore_cluster[0].id : null
+  datastore_id           = var.vsphere_datastore != "" ? data.vsphere_datastore.datastore[0].id : null
+  num_cpus               = var.vsphere_virtual_machine_cpu_count
+  num_cores_per_socket   = var.num_cores_per_socket
+  cpu_hot_add_enabled    = var.cpu_hot_add_enabled
+  cpu_hot_remove_enabled = var.cpu_hot_remove_enabled
+  memory                 = var.vsphere_virtual_machine_memory_size
+  memory_hot_add_enabled = var.memory_hot_add_enabled
+  guest_id               = data.vsphere_virtual_machine.template.guest_id
+  scsi_type              = data.vsphere_virtual_machine.template.scsi_type
+
+  dynamic "network_interface" {
+    for_each = var.network_cards
+    content {
+      network_id   = data.vsphere_network.network[network_interface.key].id
+      adapter_type = data.vsphere_virtual_machine.template.network_interface_types[0]
+    }
   }
-  disk {
-    label = "disk0"
-    size  = 20
-    thin_provisioned = data.vsphere_virtual_machine.source_template.disks[0].thin_provisioned
+
+  // Disks defined in the original template
+  dynamic "disk" {
+    for_each = data.vsphere_virtual_machine.template.disks
+    iterator = template_disks
+    content {
+      label            = "disk${template_disks.key}"
+      size             = data.vsphere_virtual_machine.template.disks[template_disks.key].size
+      unit_number      = template_disks.key
+      thin_provisioned = data.vsphere_virtual_machine.template.disks[template_disks.key].thin_provisioned
+      eagerly_scrub    = data.vsphere_virtual_machine.template.disks[template_disks.key].eagerly_scrub
+    }
+  }
+
+  clone {
+    template_uuid = data.vsphere_virtual_machine.template.id
+    customize {
+      linux_options {
+        host_name = var.vsphere_virtual_machine_name
+        domain    = var.domain
+      }
+
+      dynamic "network_interface" {
+        for_each = var.network_cards
+        content {
+          ipv4_address = cidrhost(var.virtual_machine_network_address, var.virtual_machine_ip_address_start + count.index)
+          ipv4_netmask = "%{if local.interface_count == 1}${var.ipv4_submask[0]}%{else}${var.ipv4_submask[network_interface.key]}%{endif}"
+        }
+      }
+
+      dns_server_list = var.vm_dns
+      ipv4_gateway    = var.vm_gateway
+    }
   }
 }
